e14ac739ab5039f7979cc558b9c263ff75ce86a0
changkiyun <ekzm7845@naver.com>
2025-02-17 17:03:48 +0900

    feat: state관리리

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -11,6 +11,7 @@ class MyComponent extends Component {
             children 값은 {children} 입니다.
             <br />
             가장 좋아하는 숫자는 {favoriteNumber} 입니다.
+            가장 좋아하는 숫자는 {favoriteNumber} 입니다.
          </div>
       );
    }
