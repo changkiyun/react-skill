32638630f8ad6ca5ebb2b94d89dce5eaaeafbd13
changkiyun <ekzm7845@naver.com>
2025-02-28 16:04:52 +0900

    commit

--- todo-app/src/components/TodoTemplate.js
+++ todo-app/src/components/TodoTemplate.js
@@ -4,7 +4,7 @@ import './TodoTemplate.scss'
 const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
-      <div className="app-title">일정 관리 관리리리리</div>
+      <div className="app-title">일정 관리 관리리리리리리</div>
       <div className="content">{children}</div>
       <div></div>
       <div>TEST</div>
